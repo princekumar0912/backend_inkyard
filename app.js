8d27b4f9041f3c3b3b26e9c547509f9a1ff6c1db
princekumar0912 <princerajkumar065@gmail.com>
2024-11-25 00:57:58 +0530

    remove originlink

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(cors({
-  origin: "https://inkyardtattoo.vercel.app", // Replace with your Vercel URL
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true,
-}));
 
 // app.use(cors({ origin: "https://inkyardtattoo.vercel.app" }));
 //Mongodb connection
