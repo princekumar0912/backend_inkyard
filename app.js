a372d59dd1651bb00a145ec5cc77e30917d03e59
princekumar0912 <princerajkumar065@gmail.com>
2024-11-25 00:35:13 +0530

    origin forntend link

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(cors({ origin: "https://inkyardtattoo.vercel.app" }));
 //Mongodb connection
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('MongoDB connected'))
